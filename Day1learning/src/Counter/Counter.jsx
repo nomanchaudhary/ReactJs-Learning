d2294cfbd79dd4a2fc18c7547ecef6f5b358d043
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-02-13 15:33:42 +0500

    React Learning

---
+++ Day1learning/src/Counter/Counter.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Counter = () => {
+let [counter , setCounter] = useState(0)
+ const increment = ()=>{
+    setCounter(counter+1)
+ };
+ const decrement = ()=>{
+    setCounter(counter-1)
+ };
+ const reset = ()=>{
+    setCounter(0)
+ };
+  return (
+    <div>
+      <h1>Counter value :{counter}</h1><br />
+      <button onClick={increment}>Increment</button><br />
+      <button onClick={decrement}>Decrement</button><br />
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default Counter
