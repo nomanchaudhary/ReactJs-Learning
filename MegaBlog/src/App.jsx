672aa04521b2ba8f092791d8028dc386d3c0e056
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-03-15 02:55:41 +0500

    Blog App in React Js with Appwrite

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <div>
-      <h1>Blog App</h1>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet/> */}</main>
+        <Footer />
+      </div>
     </div>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
---
+++ MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function logo() {
+  return <div>Logo</div>;
+}
+
+export default logo;
