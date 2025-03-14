cc0adba11c2c7d908b4c6129d2b86fc95a25b213
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-03-14 23:50:41 +0500

    Blog App in React Js with Appwrite

---
+++ MegaBlog/src/appwrite/database.js
@@ -0,0 +1,110 @@
+import config from "../config/config";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  storage;
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.storage = new Storage(this.client);
+  }
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          status,
+          featuredImage,
+        }
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getAllPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  //Upload File Services
+  async uploadFile(file) {
+    try {
+      return await this.storage.createFile(
+        config.appwriteStorageId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async deleteFile(fileId) {
+    try {
+      return await this.storage.deleteFile(config.appwriteStorageId, fileId);
+      return true;
+    } catch (error) {
+      throw error;
+    }
+    return false;
+  }
+  async previewFile(fileId) {
+    return this.storage.getFilePreview(config.appwriteStorageId, fileId);
+  }
+}
+
+const service = new Service();
+export default service;
