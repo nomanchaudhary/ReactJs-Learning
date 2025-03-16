2d2577fd48ad322e65611d0558f4d09a0412b39d
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-03-16 16:29:09 +0500

    Blog App in React with Appwrite

---
+++ MegaBlog/src/components/SignupComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Signup } from './Signup.jsx';
+
+const SignupComponent = () => {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  );
+};
+
+export default SignupComponent;
--- MegaBlog/src/store/store.jsx
+++ MegaBlog/src/store/store.jsx
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice.jsx"
 
 
 const store = configureStore({
-    reducer : {}
+    reducer : {
+        auth : authSlice
+    }
 })
 
 export default store;
